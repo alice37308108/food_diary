01093f2e83d3141b0ebb38a512327b8bd1248f2f
alice <minion.banana.poteto2013@gmail.com>
2024-11-20 22:38:46 +0900

    gratitude_list.htmlにページネーションを設定

--- gratitude/views.py
+++ gratitude/views.py
@@ -14,7 +14,7 @@ class GratitudePhraseListView(ListView):
     model = GratitudePhrase
     template_name = 'gratitude/gratitude_list.html'  # 使用するテンプレート名
     context_object_name = 'phrases'  # テンプレート内で使う変数名
-    paginate_by = 10 # 1ページに表示する件数
+    paginate_by = 5 # 1ページに表示する件数
 
     def get_queryset(self):
         # URLからカテゴリーのIDを取得
